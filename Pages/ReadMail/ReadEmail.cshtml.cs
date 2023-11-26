7f43a16c03a7ac16387f94a4486f1340ce62838b
JIRANUWAT MUANGSAENG <jiranuwat.muan@bu.ac.th>
2023-11-27 01:58:00 +0700

    minichange

--- Pages/ReadMail/ReadEmail.cshtml.cs
+++ Pages/ReadMail/ReadEmail.cshtml.cs
@@ -38,8 +38,21 @@ namespace FinalProject.Pages
                                     EmailIsRead = reader.GetString(4),
                                     EmailSender = reader.GetString(5),
                                     EmailReceiver = reader.GetString(6)
-                                };//ตรงนี้เป็นการนำข้อมูลจากในDatabaseมาเก็บไว้ในตัวแปรต่างๆผ่านMethod .Get ต่างๆ
-                                OnPost();//เรียกใช้ Function OnPost เพื่อเป็นการUPDATE Database ในส่วนของ emailsread
+                                };//ตรงนี้เป็นการนำข้อมูลจากในDatabaseมาเก็บไว้ในตัวแปรต่างๆผ่านMethod .Get ต่าง 
+                                    String upconnectionString = "Server=tcp:acdcproject.database.windows.net,1433;Initial Catalog=ACDC;Persist Security Info=False;User ID=acdc;Password=@Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+                                    using (SqlConnection upconnection = new SqlConnection(upconnectionString))//ประกาศmethod ในการ connect กับ database
+                                    {
+                                        upconnection.Open();//เรียกใช้ Method connection ในการเชื่อต่อ database
+
+                                        String upsql = "UPDATE emails SET emailisread=@read WHERE emailid=@emailid";//ประกาศตัว คำสั่งsql ให้อัพเดตหรือเปลี่ยนแปลงข้อในColum emailisread โดยผ่านemailsidของ emailที่จะ อัพเดต
+                                        using (SqlCommand upcommand = new SqlCommand(upsql, upconnection))//ประกาศ method SqlCommand เพื่อที่จะรัน CommandของSql
+                                        {
+                                            upcommand.Parameters.AddWithValue("@emailid", id);//เอา id เข้าไปแทน @emailid ในคำสั่งของSql
+                                            upcommand.Parameters.AddWithValue("@read", 1);//เอาค่า 1 เข้าไปแทน @read ในคำสั่้งของ Sql
+                                            upcommand.ExecuteNonQuery();//เป็นการรันCommand ของ Sql ทั้งหมด เพื่อที่จะอัพเดต
+
+                                        }
+                                    };
                             }
                          
 
@@ -54,28 +67,6 @@ namespace FinalProject.Pages
         }
         public void OnPost()//ประกาศเป็นฟังก์ชั่น เพื่อที่จะเอาไปเรียกใช้ใน Onget()
         {
-            string id = Request.Query["emailid"];//เอา id มาเก็บค่า id ของ email ผ่าน Request.Query จาก Index
-            try
-            {
-                String connectionString = "Server=tcp:acdcproject.database.windows.net,1433;Initial Catalog=ACDC;Persist Security Info=False;User ID=acdc;Password=@Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-                using (SqlConnection connection = new SqlConnection(connectionString))//ประกาศmethod ในการ connect กับ database
-                {
-                    connection.Open();//เรียกใช้ Method connection ในการเชื่อต่อ database
-
-                    String sql = "UPDATE emails SET emailisread=@read WHERE emailid=@emailid";//ประกาศตัว คำสั่งsql ให้อัพเดตหรือเปลี่ยนแปลงข้อในColum emailisread โดยผ่านemailsidของ emailที่จะ อัพเดต
-                    using (SqlCommand command = new SqlCommand(sql, connection))//ประกาศ method SqlCommand เพื่อที่จะรัน CommandของSql
-                    {
-                        command.Parameters.AddWithValue("@emailid", id);//เอา id เข้าไปแทน @emailid ในคำสั่งของSql
-                        command.Parameters.AddWithValue("@read", 1);//เอาค่า 1 เข้าไปแทน @read ในคำสั่้งของ Sql
-                        command.ExecuteNonQuery();//เป็นการรันCommand ของ Sql ทั้งหมด เพื่อที่จะอัพเดต
-
-                    }
-                };
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
         }
         public class EmailInfo
         {
