87c320d291c59b5bfe69886e8595e3c1b4a67e0f
JIRANUWAT MUANGSAENG <jiranuwat.muan@bu.ac.th>
2023-11-27 03:14:17 +0700

    ReceiverCheck in database

--- Pages/ComposeMail/ComposeEmail.cshtml.cs
+++ Pages/ComposeMail/ComposeEmail.cshtml.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
+using System.Xml.Linq;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace FinalProject.Pages.ComposeMail
 {
     public class ComposeEmailModel : PageModel
     {
-        public String errormessage = "";
-        public String successmessage = "";
-
+        public string ErrorMessage = "";
+        public string SuccessMessage = "";
+        public List<Name> ListName = new List<Name>();
         public void OnGet()
         {
         }
@@ -19,31 +21,53 @@ namespace FinalProject.Pages.ComposeMail
             email.EmailSubject = Request.Form["subject"];
             email.EmailMessage = Request.Form["message"];
                 try
-            {
+           {
                 String connectionString = "Server=tcp:acdcproject.database.windows.net,1433;Initial Catalog=ACDC;Persist Security Info=False;User ID=acdc;Password=@Admin123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
                 using (SqlConnection connection = new SqlConnection(connectionString))//ประกาศmethod ในการ connect กับ database
                 {
                     connection.Open();//เรียกใช้ Method connection ในการเชื่อต่อ database
 
-                    String sql = "INSERT INTO emails" +
-                        "(emailsubject,emailmessage,emailisread,emailsender,emailreceiver)VALUES" +
-                        "(@subject,@message,@read,@sender,@receiver);"; ;//ประกาศตัว คำสั่งsql ให้เรียกข้อมูลจากdatabase โดยเอาข้อมูลของ emailid นั้นๆ
-                    using (SqlCommand command = new SqlCommand(sql, connection))//ประกาศ method SqlCommand เพื่อที่จะรัน CommandของSql
+                    String sql = "SELECT UserName FROM AspNetUsers ";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
                     {
-                        command.Parameters.AddWithValue("@subject", email.EmailSubject);
-                        command.Parameters.AddWithValue("@message", email.EmailMessage);
-                        command.Parameters.AddWithValue("@read", 0);
-                        command.Parameters.AddWithValue("@sender", sender);
-                        command.Parameters.AddWithValue("@receiver",email.EmailReceiver);
-                        command.ExecuteNonQuery();
-
-
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Name name = new Name
+                                {
+                                    name = reader.GetString(0)
+                                };
+                                ListName.Add(name);
+                            }
+                        }
                     }
+                    if (ListName.Exists(n => n.name == email.EmailReceiver))//cheack receiver name that is in database ?
+                    {
+                        string insertSql = "INSERT INTO emails (emailsubject, emailmessage, emailisread, emailsender, emailreceiver) " +
+                                           "VALUES (@subject, @message, @read, @sender, @receiver)";
+                        using (SqlCommand insertCommand = new SqlCommand(insertSql, connection))
+                        {
+                            insertCommand.Parameters.AddWithValue("@subject", email.EmailSubject);
+                            insertCommand.Parameters.AddWithValue("@message", email.EmailMessage);
+                            insertCommand.Parameters.AddWithValue("@read", 0);
+                            insertCommand.Parameters.AddWithValue("@sender", sender);
+                            insertCommand.Parameters.AddWithValue("@receiver", email.EmailReceiver);
+                            insertCommand.ExecuteNonQuery();
+                        }
+
+                        SuccessMessage = "Email sent successfully!";
+                    }
+                    else
+                    {
+                        ErrorMessage = "UserName Not Found.";
+                    }
+
                 };
             }
             catch (Exception ex)
             {
-                errormessage = ex.Message;
+                ErrorMessage = $"An error occurred: {ex.Message}";
                 return;
             }
             email.EmailReceiver = "";
@@ -60,5 +84,9 @@ namespace FinalProject.Pages.ComposeMail
         public String EmailIsRead;
         public String EmailSender;
         public String EmailReceiver;
+    }
+    public class Name
+    {
+        public String name;
     }
 }
