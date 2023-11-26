a308a7058f64465d6d4ee318d0d29fa1887c5558
JIRANUWAT MUANGSAENG <jiranuwat.muan@bu.ac.th>
2023-11-26 23:19:39 +0700

    Deleted Bug Fixed#Jiranuwat

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,7 @@
-﻿SELECT * FROM emails
+﻿INSERT INTO emails (emailsubject, emailmessage, emailisread, emailsender, emailreceiver) VALUES
+
+('Morning','I hope to see you in the meeting today. We need to talk in so many topics.','0','Emily','Jack'),
+
+('Meeting today?','New issues have come up yesterday during the testing. Shall we have a short meeting today after lunch?','0','Jack','Emily'),
+
+('Welcome to the team','I believe we have not yet chat. My name is Supat. Do you have 10 minutes at 2?','0','Supat','Emily');
